7d2097488ec83d2debe55367dcd9deaadd1c5ba7
AAYUSHMAN MAC <aayushmanmac@AAYUSHMANs-Mac-mini.local>
2025-09-21 14:49:58 +0530

    Implemented the Inbuilt Login Feature

--- blogproject/blogproject/urls.py
+++ blogproject/blogproject/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+
+
 ]
